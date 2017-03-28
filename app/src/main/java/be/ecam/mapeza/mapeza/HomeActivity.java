ad044d562a09fce18cfd65132ea25f491395d94f
12415 <thibautr@hotmail.be>
2017-03-28 10:37:48 +0200

    Thib commit

--- app/src/main/java/be/ecam/mapeza/mapeza/HomeActivity.java
+++ app/src/main/java/be/ecam/mapeza/mapeza/HomeActivity.java
@@ -1,17 +1,65 @@
 package be.ecam.mapeza.mapeza;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    // String placePreference; = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //THIS IS A TEST
-        //THIS IS A SECOND TEST
-        //THIS IS A THIRD TEST
 
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        /*
+        doUsefulStuffBasedOnMyPreference(
+        sharedPreferences.getString("tempunit", "celsius"));
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        */
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.mapeza_menu,menu);
+        return true;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+    {
+        //doUsefulStuffBasedOnMyPreference(sharedPreferences.getString(key, "bars"));
+    }
+
+    private void doUsefulStuffBasedOnMyPreference(String placePreference)
+    {
+        //this.placePreference = placePreference;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+
+        if (itemThatWasClickedId == R.id.settings) {
+
+            Context context = this;
+            Class destinationClass = SettingsActivity.class;
+            Intent intent = new Intent(context, destinationClass);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
